9816da2251ad3a83db4a3519879b4a5f302d5cda
[pushkarprotiya] <[pushkarprotiya@gmail.com]>
2022-07-30 13:20:57 +0530

    finall complete

--- javascript.js
+++ javascript.js
@@ -274,3 +274,12 @@ $(function () {
         $(this).attr('src', 'http://tuladent.ru/assets/tpl/smc/button.png');
     });
 });
+
+
+//    preloader script 
+
+  $(window).on('load', function () { // makes sure the whole site is loaded 
+    //   $('#status').fadeOut(); // will first fade out the loading animation 
+      $('#preloader').delay(1000).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+      $('body').delay(350).css({ 'overflow': 'visible' });
+  })
